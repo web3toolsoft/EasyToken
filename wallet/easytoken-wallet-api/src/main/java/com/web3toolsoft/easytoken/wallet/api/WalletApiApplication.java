51c132c5d870bf26aa79186c7538d49d96b69e80
xianrendzw <14068728@qq.com>
2023-03-17 18:05:19 +0800

    update:project structure

---
+++ wallet/easytoken-wallet-api/src/main/java/com/web3toolsoft/easytoken/wallet/api/WalletApiApplication.java
@@ -0,0 +1,10 @@
+package com.web3toolsoft.easytoken.wallet.api;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApiApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(WalletApiApplication.class, args);
+    }
+}
---
+++ wallet/easytoken-wallet-job/src/main/java/com/web3toolsoft/easytoken/wallet/job/WalletJobApplication.java
@@ -0,0 +1,10 @@
+package com.web3toolsoft.easytoken.wallet.job;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletJobApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(WalletJobApplication.class, args);
+    }
+}
